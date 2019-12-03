a62e49b538d6fd91c28eaa1e596e902ea33bc7a6
mxc035 <malcolm.crum@ioof.com.au>
2019-12-04 10:57:03 +1100

    Create a Wire class for readability

--- src/main/kotlin/com/malcolmcrum/adventofcode2019/Day3.kt
+++ src/main/kotlin/com/malcolmcrum/adventofcode2019/Day3.kt
@@ -4,56 +4,67 @@ import java.io.File
 import java.util.*
 import kotlin.math.abs
 
-fun nearestIntersection(a: Map<Point, Int>, b: Map<Point, Int>): Point {
-    return a.keys
-        .filter { it != Point.START }
-        .filter { b.contains(it) }
+fun nearestIntersection(a: Wire, b: Wire): Point {
+    return a.points
+        .filter { point -> b.intersects(point) }
         .minBy { it.manhattanDistance }!!
 }
 
-fun shortestStepsToIntersection(a: Map<Point, Int>, b: Map<Point, Int>): Int {
-    return a
-        .filter { (point, _) -> b.contains(point) }
-        .map { (point, steps) -> steps + b.getValue(point) }
+fun shortestStepsToIntersection(a: Wire, b: Wire): Int {
+    return a.points
+        .filter { point -> b.intersects(point) }
+        .map { point -> a.stepsTo(point) + b.stepsTo(point) }
         .min()!!
 }
 
-fun parseWire(steps: String): Map<Point, Int> {
-    val points = mutableMapOf<Point, Int>()
-    var x = 0
-    var y = 0
-    var stepCount = 0
-    for (step in steps.split(",")) {
-        val instruction = step[0]
-        val count = step.substring(1).toInt()
-        assert(count >= 0)
-        repeat(count) {
-            stepCount++
-            when (instruction) {
-                'U' -> y++
-                'D' -> y--
-                'L' -> x--
-                'R' -> x++
-                else -> throw InputMismatchException("Invalid instruction $instruction")
+
+
+class Wire(private val steps: Map<Point, Int>) {
+    fun intersects(point: Point): Boolean {
+        return steps.containsKey(point)
+    }
+
+    fun stepsTo(point: Point): Int {
+        return steps.getValue(point)
+    }
+
+    val points = steps.keys
+
+    companion object {
+        fun parse(steps: String): Wire {
+            val points = mutableMapOf<Point, Int>()
+            var x = 0
+            var y = 0
+            var stepCount = 0
+            for (step in steps.split(",")) {
+                val instruction = step[0]
+                val count = step.substring(1).toInt()
+                assert(count >= 0)
+                repeat(count) {
+                    stepCount++
+                    when (instruction) {
+                        'U' -> y++
+                        'D' -> y--
+                        'L' -> x--
+                        'R' -> x++
+                        else -> throw InputMismatchException("Invalid instruction $instruction")
+                    }
+                    points.putIfAbsent(Point(x, y), stepCount)
+                }
             }
-            points.putIfAbsent(Point(x, y), stepCount)
+            return Wire(points)
         }
     }
-    return points
 }
 
 data class Point(val x: Int, val y: Int) {
     val manhattanDistance = abs(x) + abs(y)
-
-    companion object {
-        val START = Point(0, 0)
-    }
 }
 
 fun main() {
     val data = File("src/main/resources/input/day3.txt").readLines()
-    val wire1 = parseWire(data[0])
-    val wire2 = parseWire(data[1])
+    val wire1 = Wire.parse(data[0])
+    val wire2 = Wire.parse(data[1])
 
     val intersection = nearestIntersection(wire1, wire2)
 
--- src/test/kotlin/com/malcolmcrum/adventofcode2019/Day3Test.kt
+++ src/test/kotlin/com/malcolmcrum/adventofcode2019/Day3Test.kt
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Test
 internal class Day3Test {
     @Test
     fun `simple test`() {
-        val wire1 = parseWire("R8,U5,L5,D3")
-        val wire2 = parseWire("U7,R6,D4,L4")
+        val wire1 = Wire.parse("R8,U5,L5,D3")
+        val wire2 = Wire.parse("U7,R6,D4,L4")
 
         val intersection = nearestIntersection(wire1, wire2)
 
@@ -18,8 +18,8 @@ internal class Day3Test {
 
     @Test
     fun `complex test`() {
-        val wire1 = parseWire("R75,D30,R83,U83,L12,D49,R71,U7,L72")
-        val wire2 = parseWire("U62,R66,U55,R34,D71,R55,D58,R83")
+        val wire1 = Wire.parse("R75,D30,R83,U83,L12,D49,R71,U7,L72")
+        val wire2 = Wire.parse("U62,R66,U55,R34,D71,R55,D58,R83")
 
         val intersection = nearestIntersection(wire1, wire2)
 
@@ -28,8 +28,8 @@ internal class Day3Test {
 
     @Test
     fun `another complex test`() {
-        val wire1 = parseWire("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
-        val wire2 = parseWire("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+        val wire1 = Wire.parse("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
+        val wire2 = Wire.parse("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
 
         val intersection = nearestIntersection(wire1, wire2)
 
@@ -38,26 +38,26 @@ internal class Day3Test {
 
     @Test
     fun `verify simple wire parsing`() {
-        val wire = parseWire("R1")
-        assertThat(wire.keys).containsOnly( Point(1, 0))
+        val wire = Wire.parse("R1")
+        assertThat(wire.points).containsOnly( Point(1, 0))
     }
 
     @Test
     fun `verify wire parsing with multiple steps`() {
-        val wire = parseWire("R1,U1")
-        assertThat(wire.keys).containsOnly(Point(1, 0), Point(1, 1))
+        val wire = Wire.parse("R1,U1")
+        assertThat(wire.points).containsOnly(Point(1, 0), Point(1, 1))
     }
 
     @Test
     fun `verify wire parsing with overlap`() {
-        val wire = parseWire("R1,U1,L1,D1")
-        assertThat(wire.keys).containsOnly(Point(1, 0), Point(1, 1), Point(0, 1), Point(0, 0))
+        val wire = Wire.parse("R1,U1,L1,D1")
+        assertThat(wire.points).containsOnly(Point(1, 0), Point(1, 1), Point(0, 1), Point(0, 0))
     }
 
     @Test
     fun `verify step parsing`() {
-        val wire1 = parseWire("R75,D30,R83,U83,L12,D49,R71,U7,L72")
-        val wire2 = parseWire("U62,R66,U55,R34,D71,R55,D58,R83")
+        val wire1 = Wire.parse("R75,D30,R83,U83,L12,D49,R71,U7,L72")
+        val wire2 = Wire.parse("U62,R66,U55,R34,D71,R55,D58,R83")
 
         val steps = shortestStepsToIntersection(wire1, wire2)
 
