89d7d58a1c2ab956cd1eaf20daca51f6f8428267
mxc035 <malcolm.crum@ioof.com.au>
2019-12-06 11:45:35 +1100

    Day 5 part 2

--- src/main/kotlin/com/malcolmcrum/adventofcode2019/Day5.kt
+++ src/main/kotlin/com/malcolmcrum/adventofcode2019/Day5.kt
@@ -3,6 +3,7 @@ package com.malcolmcrum.adventofcode2019
 import java.io.File
 
 class Emulator2(private val data: IntArray) {
+    private val originalData = data.copyOf()
     private var input = mutableListOf<Int>()
     private var pc: Int = 0
     private val output = mutableListOf<Int>()
@@ -10,51 +11,101 @@ class Emulator2(private val data: IntArray) {
     fun tick(): Operation {
         val instruction = parseInstruction(data[pc])
         val operation = instruction.operation
-        when (operation) {
+        val jumpAddress = when (operation) {
             Operation.Add -> add(instruction)
             Operation.Multiply -> multiply(instruction)
             Operation.ReadInput -> readInput(instruction)
             Operation.WriteOutput -> writeOutput(instruction)
-            Operation.End -> {
-            }
+            Operation.JumpIfTrue -> jumpIfTrue(instruction)
+            Operation.JumpIfFalse -> jumpIfFalse(instruction)
+            Operation.LessThan -> compareLessThan(instruction)
+            Operation.Equals -> compareEquals(instruction)
+            Operation.End -> null
         }
-        pc += operation.instructionLength
+        if (jumpAddress != null) {
+            pc = jumpAddress
+        } else {
+            pc += operation.instructionLength
+        }
         return operation
     }
 
-    private fun readInput(instruction: Instruction) {
+    private fun compareEquals(instruction: Instruction): Int? {
+        val operand1 = data[instruction.param1.invoke(pc + 1)]
+        val operand2 = data[instruction.param2.invoke(pc + 2)]
+        val result = if (operand1 == operand2) 1 else 0
+        val destination = instruction.param3.invoke(pc + 3)
+        println("data[$destination] = $operand1 == $operand2 = $result")
+        data[destination] = result
+        return null
+    }
+
+    private fun compareLessThan(instruction: Instruction): Int? {
+        val operand1 = data[instruction.param1.invoke(pc + 1)]
+        val operand2 = data[instruction.param2.invoke(pc + 2)]
+        val result = if (operand1 < operand2) 1 else 0
+        val destination = instruction.param3.invoke(pc + 3)
+        println("data[$destination] = $operand1 < $operand2 = $result")
+        data[destination] = result
+        return null
+    }
+
+    private fun jumpIfFalse(instruction: Instruction): Int? {
+        val check = data[instruction.param1.invoke(pc + 1)]
+        return if (check == 0) {
+            data[instruction.param2.invoke(pc + 2)]
+        } else {
+            null
+        }
+    }
+
+    private fun jumpIfTrue(instruction: Instruction): Int? {
+        val check = data[instruction.param1.invoke(pc + 1)]
+        return if (check != 0) {
+            data[instruction.param2.invoke(pc + 2)]
+        } else {
+            null
+        }
+    }
+
+    private fun readInput(instruction: Instruction): Int? {
         val destination = instruction.param1.invoke(pc + 1)
         val value = input.removeAt(0)
         println("data[$destination] = $value from input")
         data[destination] = value
+        return null
     }
 
-    private fun multiply(instruction: Instruction) {
+    private fun multiply(instruction: Instruction): Int? {
         val operand1 = data[instruction.param1.invoke(pc + 1)]
         val operand2 = data[instruction.param2.invoke(pc + 2)]
         val destination = instruction.param3.invoke(pc + 3)
         println("data[$destination] = $operand1 * $operand2")
         data[destination] = operand1 * operand2
+        return null
     }
 
-    private fun add(instruction: Instruction) {
+    private fun add(instruction: Instruction): Int? {
         val operand1 = data[instruction.param1.invoke(pc + 1)]
         val operand2 = data[instruction.param2.invoke(pc + 2)]
         val destination = instruction.param3.invoke(pc + 3)
         println("data[$destination] = $operand1 + $operand2")
         data[destination] = operand1 + operand2
+        return null
     }
 
-    private fun writeOutput(instruction: Instruction) {
+    private fun writeOutput(instruction: Instruction): Int? {
         val address = instruction.param1.invoke(pc + 1)
         val value = data[address]
         println("output += data[$address] ($value)")
         output.add(value)
+        return null
     }
 
     fun run(input: List<Int> = listOf()): List<Int> {
         pc = 0
         output.clear()
+        originalData.copyInto(data)
         this.input = input.toMutableList()
         do {
             val operation = tick()
@@ -65,11 +116,12 @@ class Emulator2(private val data: IntArray) {
     fun parseInstruction(instruction: Int): Instruction {
         val padded = instruction.toString().padStart(5, '0')
         val (A, B, C, DE) = "(\\d)(\\d)(\\d)(\\d\\d)".toRegex().matchEntire(padded)!!.destructured
-        println("Parsed $padded to $A, $B, $C, $DE")
+        print("Parsed $padded to $A, $B, $C, $DE: ")
         val param1 = readParameter(C.toInt())
         val param2 = readParameter(B.toInt())
         val param3 = readParameter(A.toInt())
         val operation = parseOperation(DE)
+        println(operation)
         return Instruction(operation, param1, param2, param3)
     }
 
@@ -86,6 +138,10 @@ class Emulator2(private val data: IntArray) {
         object Multiply : Operation(2, 4)
         object ReadInput : Operation(3, 2)
         object WriteOutput : Operation(4, 2)
+        object JumpIfTrue : Operation(5, 3)
+        object JumpIfFalse : Operation(6, 3)
+        object LessThan : Operation(7, 4)
+        object Equals : Operation(8, 4)
         object End : Operation(99, 0)
     }
 
@@ -95,6 +151,10 @@ class Emulator2(private val data: IntArray) {
             Operation.Multiply.code -> Operation.Multiply
             Operation.ReadInput.code -> Operation.ReadInput
             Operation.WriteOutput.code -> Operation.WriteOutput
+            Operation.JumpIfTrue.code -> Operation.JumpIfTrue
+            Operation.JumpIfFalse.code -> Operation.JumpIfFalse
+            Operation.LessThan.code -> Operation.LessThan
+            Operation.Equals.code -> Operation.Equals
             Operation.End.code -> Operation.End
             else -> throw IllegalArgumentException("Illegal operation $opcode")
         }
@@ -114,10 +174,14 @@ class Emulator2(private val data: IntArray) {
 }
 
 fun main() {
-    val originalInstructions = File("src/main/resources/input/day5.txt")
+    val instructions = File("src/main/resources/input/day5.txt")
         .readLines().flatMap { it.split(",") }.map { it.toInt() }.toIntArray()
-    val emulator = Emulator2(originalInstructions)
+    val emulator = Emulator2(instructions)
     val output = emulator.run(listOf(1))
 
     println(output)
+
+    val output2 = emulator.run(listOf(5))
+
+    println(output2)
 }
--- src/test/kotlin/com/malcolmcrum/adventofcode2019/Day5Test.kt
+++ src/test/kotlin/com/malcolmcrum/adventofcode2019/Day5Test.kt
@@ -19,13 +19,92 @@ internal class Day5Test {
     fun `add values, immediate addressing`() {
         val addInstructions = intArrayOf(
             1101, 2, 3, 0, // [0] = 2 + 3
-            4, 0, // read [0]
+            4, 0, // output [0]
             99 // end
         )
         val emulator = Emulator2(addInstructions)
         val output = emulator.run()
 
         assertThat(output).containsOnly(5)
+    }
+
+    @Test
+    fun `position mode, check equal to 8`() {
+        val equals8Instructions = intArrayOf(
+            3, 9, // [9] = input
+            8, 9, 10, 9, // [9] = [9] == [10]
+            4, 9, // output [9]
+            99, // end
+            -1, // [9] - result is stored here
+            8 // [10] - comparison value
+        )
+        val emulator = Emulator2(equals8Instructions)
+        assertThat(emulator.run(listOf(8))).containsOnly(1)
+        assertThat(emulator.run(listOf(1))).containsOnly(0)
+    }
+
+    @Test
+    fun `position mode, check less than 8`() {
+        val lessThan8Instructions = intArrayOf(
+            3, 9, // [9] = input
+            7, 9, 10, 9, // [9] = [9] < [10]
+            4, 9, // output [9]
+            99, // end
+            -1, // [9] - result is stored here
+            8 // [10] - comparison value
+        )
+        val emulator = Emulator2(lessThan8Instructions)
+        assertThat(emulator.run(listOf(8))).containsOnly(0)
+        assertThat(emulator.run(listOf(7))).containsOnly(1)
+    }
+
+    @Test
+    fun `immediate mode, check less than 8`() {
+        val lessThan8Instructions = intArrayOf(
+            3, 3, // [9] = input
+            1107, -1, 8, 3, // [3] = -1 < 8
+            4, 3, // output [3]
+            99 // end
+        )
+        val emulator = Emulator2(lessThan8Instructions)
+        assertThat(emulator.run(listOf(8))).containsOnly(0)
+        assertThat(emulator.run(listOf(7))).containsOnly(1)
+    }
+
+    @Test
+    fun `large example`() {
+        val instructions = intArrayOf(
+            3, 21, // [21] = input
+            1008, 21, 8, 20, // [20] = [21] == 8
+            1005, 20, 22, // if [20] != 0 jump to [22]
+            107, 8, 21, 20, 1006, 20, 31,
+            1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+            999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+        )
+        val emulator = Emulator2(instructions)
+
+        assertThat(emulator.run(listOf(5))).containsOnly(999)
+        assertThat(emulator.run(listOf(8))).containsOnly(1000)
+        assertThat(emulator.run(listOf(55))).containsOnly(1001)
+    }
+
+    @Test
+    fun `jump test 1`() {
+        val jumpTest = intArrayOf(
+            3, 12, // [12] = input
+            6, 12, 15, // if [12] != 0 jump to [15] (9)
+            1, 13, 14, 13, // [13] = [13] + [14]
+            4, 13, // output [13] (0)
+            99, // end
+            -1, // [12]
+            0, // [13]
+            1, // [14]
+            9 // [15]
+        )
+        val emulator = Emulator2(jumpTest)
+        assertThat(emulator.run(listOf(0))).containsOnly(0)
+        assertThat(emulator.run(listOf(1))).containsOnly(1)
+        assertThat(emulator.run(listOf(2))).containsOnly(1)
     }
 
 }
