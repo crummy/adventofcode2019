4a0ab4e70bc120d5bab4e6e38c9767e5e929a4a5
mxc035 <malcolm.crum@ioof.com.au>
2019-12-11 11:12:15 +1100

    Day 10 part 2, spent too long on this and it was just stupid array indexing

--- src/main/kotlin/com/malcolmcrum/adventofcode2019/Day10.kt
+++ src/main/kotlin/com/malcolmcrum/adventofcode2019/Day10.kt
@@ -1,13 +1,16 @@
 package com.malcolmcrum.adventofcode2019
 
 import java.io.File
+import kotlin.math.PI
 import kotlin.math.atan2
+import kotlin.math.pow
 
 typealias Asteroid = Pair<Int, Int>
 
 class AsteroidBelt(val asteroids: Set<Asteroid>) {
-    fun maxVisibleAsteroids(): Int {
-        return asteroids.map { asteroidsVisible(it) }.max()!!
+    fun maxVisibleAsteroids(): Pair<Asteroid, Int> {
+        return asteroids.map { it to asteroidsVisible(it) }
+            .maxBy { it.second }!!
     }
 
     private fun asteroidsVisible(asteroid: Asteroid): Int {
@@ -17,8 +20,34 @@ class AsteroidBelt(val asteroids: Set<Asteroid>) {
             .count()
     }
 
+    fun vaporizeAsteroids(asteroid: Asteroid): List<Asteroid> {
+        val asteroidAngles = asteroids.filter { it != asteroid }
+            .groupBy { it.angleTo(asteroid) }
+            .map { (angle, asteroids) -> angle to asteroids.sortedBy { it.distanceSquared(asteroid) }.toMutableList() }
+            .toMap()
+
+        val vaporizedAsteroids = mutableListOf<Asteroid>()
+        while (vaporizedAsteroids.size < asteroids.size - 1) { // we don't destroy the one we're sitting on
+            for (angle in asteroidAngles.keys.sorted()) {
+                val asteroidsAtAngle = asteroidAngles.getValue(angle)
+                if (asteroidsAtAngle.isNotEmpty()) {
+                    val vaporized = asteroidsAtAngle.removeAt(0)
+                    println("Vaporized #${vaporizedAsteroids.size}: $vaporized at angle $angle")
+                    vaporizedAsteroids.add(vaporized)
+                }
+            }
+            println("Finished sweep")
+        }
+        return vaporizedAsteroids
+    }
+
     private fun Asteroid.angleTo(other: Asteroid): Double {
-        return atan2(first - other.first.toDouble(), second - other.second.toDouble())
+        val radians = atan2(other.second - second.toDouble(), other.first - first.toDouble()) + 3*PI/2
+        return Math.toDegrees(radians) % 360
+    }
+
+    private fun Asteroid.distanceSquared(other: Asteroid): Double {
+        return (first - other.first.toDouble()).pow(2) + (second - other.second.toDouble()).pow(2)
     }
 
     companion object {
@@ -40,7 +69,12 @@ class AsteroidBelt(val asteroids: Set<Asteroid>) {
 
 fun main() {
     val input = File("src/main/resources/input/day10.txt").readLines()
-    val max = AsteroidBelt.parse(input).maxVisibleAsteroids()
+    val asteroidBelt = AsteroidBelt.parse(input)
+    val (asteroid, visible) = asteroidBelt.maxVisibleAsteroids()
 
-    println(max)
+    println(visible)
+
+    val vaporizedAsteroids = asteroidBelt.vaporizeAsteroids(asteroid)
+
+    println(vaporizedAsteroids[199].first * 100 + vaporizedAsteroids[199].second)
 }
--- src/test/kotlin/com/malcolmcrum/adventofcode2019/Day10Test.kt
+++ src/test/kotlin/com/malcolmcrum/adventofcode2019/Day10Test.kt
@@ -14,8 +14,8 @@ internal class Day10Test {
             "....#",
             "...##"
         )
-        val max = AsteroidBelt.parse(input).maxVisibleAsteroids()
-        assertThat(max).isEqualTo(8)
+        val (asteroid, visible) = AsteroidBelt.parse(input).maxVisibleAsteroids()
+        assertThat(visible).isEqualTo(8)
     }
 
     @Test
@@ -42,7 +42,62 @@ internal class Day10Test {
             "#.#.#.#####.####.###",
             "###.##.####.##.#..##"
         )
-        val max = AsteroidBelt.parse(input).maxVisibleAsteroids()
-        assertThat(max).isEqualTo(210)
+        val (asteroid, visible) = AsteroidBelt.parse(input).maxVisibleAsteroids()
+        assertThat(visible).isEqualTo(210)
+    }
+
+    @Test
+    fun `vaporization test`() {
+        val input = listOf(
+            ".#....#####...#..",
+            "##...##.#####..##",
+            "##...#...#.#####.",
+            "..#.....#...###..",
+            "..#.#.....#....##"
+        )
+
+        val belt = AsteroidBelt.parse(input)
+        val (site, _) = belt.maxVisibleAsteroids()
+        val vaporized = belt.vaporizeAsteroids(site)
+
+        assertThat(vaporized[0]).isEqualTo(Asteroid(8, 1))
+        assertThat(vaporized[1]).isEqualTo(Asteroid(9, 0))
+        assertThat(vaporized[2]).isEqualTo(Asteroid(9, 1))
+        assertThat(vaporized[8]).isEqualTo(Asteroid(15, 1))
+        val asteroidCount = input.joinToString().count { it == '#' }
+        assertThat(vaporized.size).isEqualTo(asteroidCount - 1) // exclude our own one
+    }
+
+    @Test
+    fun `big vaporization test`() {
+        val input = listOf(
+            ".#..##.###...#######",
+            "##.############..##.",
+            ".#.######.########.#",
+            ".###.#######.####.#.",
+            "#####.##.#.##.###.##",
+            "..#####..#.#########",
+            "####################",
+            "#.####....###.#.#.##",
+            "##.#################",
+            "#####.##.###..####..",
+            "..######..##.#######",
+            "####.##.####...##..#",
+            ".#####..#.######.###",
+            "##...#.##########...",
+            "#.##########.#######",
+            ".####.#.###.###.#.##",
+            "....##.##.###..#####",
+            ".#.#.###########.###",
+            "#.#.#.#####.####.###",
+            "###.##.####.##.#..##"
+        )
+        val belt = AsteroidBelt.parse(input)
+        val (asteroid, _) = belt.maxVisibleAsteroids()
+        assertThat(asteroid).isEqualTo(Asteroid(11, 13))
+
+        val vaporized = belt.vaporizeAsteroids(asteroid)
+
+        assertThat(vaporized[199]).isEqualTo(Asteroid(8, 2))
     }
 }
