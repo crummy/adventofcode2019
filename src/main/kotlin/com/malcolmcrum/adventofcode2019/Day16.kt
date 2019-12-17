8922291e1f1aaa8389b471d12c68f5be365bb168
mxc035 <malcolm.crum@ioof.com.au>
2019-12-17 11:43:04 +1100

    Day 16 part 2. Had to get help for this one. Glad to see it done.

--- src/main/kotlin/com/malcolmcrum/adventofcode2019/Day16.kt
+++ src/main/kotlin/com/malcolmcrum/adventofcode2019/Day16.kt
@@ -3,32 +3,48 @@ package com.malcolmcrum.adventofcode2019
 import java.io.File
 import kotlin.math.abs
 
-fun fft(digits: String, phases: Int = 1): String {
+// 1. The last digit never changes.
+// 2. The value of any digit is dependent only on following digits
+// Thus, we can solve by just working backwards to our offset point
+fun findMessage(digits: String): String {
+    val offset = digits.substring(0, 7).toInt()
+    val fullDigits = digits.repeat(10000)
+    val buffer = fullDigits.map { Character.getNumericValue(it) }.toMutableList()
+    repeat(100) {
+        repeat(fullDigits.length - offset) { digit ->
+            val index = buffer.size - digit - 2 // Because last digit never changes
+            buffer[index] += buffer[index + 1]
+            buffer[index] %= 10
+        }
+    }
+    return buffer.subList(offset, offset + 8).joinToString(separator = "")
+}
+
+fun fft(digits: String, phases: Int = 1, range: IntRange = digits.indices): String {
     var input = digits.map { Character.getNumericValue(it) }
+        .subList(range.first, range.last + 1)
     repeat(phases) {
-        input = fft(input)
+        input = fft(input, range.first)
     }
     return input.joinToString(separator = "") { it.toString() }
 }
 
-fun fft(digits: List<Int>): List<Int> {
-    return digits.indices.map { outputDigit ->
-        val pattern = pattern(digits.size, outputDigit)
-        digits.mapIndexed { index, digit -> digit * pattern[index] }
+private fun fft(digits: List<Int>, initialIteration: Int): List<Int> {
+    return digits.indices.map { iteration ->
+        digits.mapIndexed { index, digit -> digit * multiplier(index, initialIteration + iteration) }
             .sum()
     }.map { abs(it % 10) }
-
 }
 
-fun pattern(length: Int, iteration: Int): List<Int> {
-    val pattern = mutableListOf<Int>()
-    do {
-        repeat(iteration + 1) { pattern += 0 }
-        repeat(iteration + 1) { pattern += 1 }
-        repeat(iteration + 1) { pattern += 0 }
-        repeat(iteration + 1) { pattern += -1 }
-    } while (pattern.size < length + 1) // +1 to offset the skip 1 on the next line
-    return pattern.subList(1, length + 1)
+fun multiplier(digit: Int, iteration: Int): Int {
+    val result = ((digit + 1) / (iteration + 1)) % (4)
+    return when (result) {
+        0 -> 0
+        1 -> 1
+        2 -> 0
+        3 -> -1
+        else -> error("odd result for $digit, $iteration")
+    }
 }
 
 fun main() {
@@ -37,4 +53,6 @@ fun main() {
     val output = fft(instructions, 100)
 
     println(output.substring(0, 8))
+
+    println(findMessage(instructions))
 }
--- src/test/kotlin/com/malcolmcrum/adventofcode2019/Day16Test.kt
+++ src/test/kotlin/com/malcolmcrum/adventofcode2019/Day16Test.kt
@@ -6,6 +6,7 @@ import assertk.assertions.startsWith
 import org.junit.jupiter.api.Test
 
 internal class Day16Test {
+
     @Test
     fun `simple test`() {
         val output = fft("12345678")
@@ -22,5 +23,34 @@ internal class Day16Test {
         assertThat(fft("80871224585914546619083218645595", 100)).startsWith("24176176")
         assertThat(fft("19617804207202209144916044189917", 100)).startsWith("73745418")
         assertThat(fft("69317163492948606335995924319873", 100)).startsWith("52432133")
+    }
+
+    @Test
+    fun `test multiplier`() {
+        repeat(8) { iteration ->
+            val pattern = pattern(16, iteration)
+            repeat(16) { digit ->
+                assertThat(multiplier(digit, iteration)).isEqualTo(pattern[digit])
+            }
+        }
+    }
+
+    @Test
+    fun `test selecting just part of a range`() {
+        assertThat(findMessage("03036732577212944063491565474664")).isEqualTo("84462026")
+        assertThat(findMessage("02935109699940807407585447034323")).isEqualTo("78725270")
+        assertThat(findMessage("03081770884921959731165446850517")).isEqualTo("53553731")
+    }
+
+    // known good, but inefficient, pattern generation
+    fun pattern(length: Int, iteration: Int): List<Int> {
+        val pattern = mutableListOf<Int>()
+        do {
+            repeat(iteration + 1) { pattern += 0 }
+            repeat(iteration + 1) { pattern += 1 }
+            repeat(iteration + 1) { pattern += 0 }
+            repeat(iteration + 1) { pattern += -1 }
+        } while (pattern.size < length + 1) // +1 to offset the skip 1 on the next line
+        return pattern.subList(1, length + 1)
     }
 }
